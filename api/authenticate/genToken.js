7ab47387918b18dc85f641b5ca3e8f8d67a632f2
yihune zewdie <zewdieyihune@gmail.com>
2024-03-18 23:38:16 +0300

    delete all the project and start agian

--- api/authenticate/genToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const { ACCESS_TOKEN } = require('../config');
-
-// generate a token
-const genToken = (id) => {
-    return jwt.sign({id}, ACCESS_TOKEN, {expiresIn: "1d"});
-};
-
-module.exports = genToken;
