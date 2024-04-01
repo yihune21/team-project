ca47e28be8fb6f72d22d8aef3535746627cde775
yihune21 <zewdieyihune@gmail.com>
2024-04-01 19:46:38 +0300

    update app.ts

--- app.ts
+++ app.ts
@@ -14,6 +14,18 @@ app.get("/", (req: any, res: any) => {
   res.send("This is the sn4y team-project API");
 });
 
+app.get("/QRScanner", (req: any, res: any) => {
+  res.send("This is the QRScanner page");
+});
+
+app.get("/contactus", (req: any, res: any) => {
+  res.send("This is the contact page");
+});
+
+app.get("/aboutus", (req: any, res: any) => {
+  res.send("This is the about us page");
+});
+
 // Start the server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
